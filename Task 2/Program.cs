d54c95cf5e5a62cf546c5a7d24adc318a059bd8b
K.Sidorov <2887458@gmail.com>
2023-02-17 23:34:22 +0300

    refactored code

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,20 +1,18 @@
 ﻿Console.WriteLine("Введите 2 целых числа: ");
-string a = Console.ReadLine();
-string b = Console.ReadLine();
 int number1;
 int number2;
 
 
-int.TryParse(a, out number1);
-int.TryParse(b, out number2);
+int.TryParse(Console.ReadLine(), out number1);
+int.TryParse(Console.ReadLine(), out number2);
 
 if (number1 > number2)
 {
-    Console.WriteLine($"Число {a} больше, чем {b}.");
+    Console.WriteLine($"Число {number1} больше, чем {number2}.");
 }
 else
 {
-    Console.WriteLine($"Число {b} больше, чем {a}.");
+    Console.WriteLine($"Число {number2} больше, чем {number1}.");
 }
 
 /*string text = Console.ReadLine();
