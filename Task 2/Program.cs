c0610f655fb83d8d6b4d4342f13416a8afecd1f1
K.Sidorov <2887458@gmail.com>
2023-02-17 23:13:07 +0300

    repaired solution for task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,8 +1,11 @@
-﻿Console.Write("Введите 2 целых числа: ");
+﻿Console.WriteLine("Введите 2 целых числа: ");
 string a = Console.ReadLine();
 string b = Console.ReadLine();
 
-if (a > b)
+int number1 = int.Parse(a);
+int number2 = int.Parse(b);
+
+if (number1 > number2)
 {
     Console.WriteLine($"Число {a} больше, чем {b}.");
 }
@@ -10,3 +13,8 @@ else
 {
     Console.WriteLine($"Число {b} больше, чем {a}.");
 }
+
+/*string text = Console.ReadLine();
+int number = int.Parse(text);
+
+Console.WriteLine(number);*/
