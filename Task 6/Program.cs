a43ac23f709768ee3628332e924ce340af430e80
K.Sidorov <2887458@gmail.com>
2023-02-17 23:58:25 +0300

    added solution for the task 6

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int number;
+int.TryParse(Console.ReadLine(), out number);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} четное");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечетное");
+}
