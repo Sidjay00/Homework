0adba73226001ff4f7e5faa2f5b078e1b90931ff
K.Sidorov <2887458@gmail.com>
2023-02-18 00:18:19 +0300

    added a solution for the task 8

--- Task 8/Program.cs
+++ Task 8/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int N;
+int.TryParse(Console.ReadLine(), out N);
+
+for (int i = 2; i <= N; i += 2)
+{
+    Console.WriteLine($"{i}");
+}
