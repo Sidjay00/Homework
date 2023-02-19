6ffedcca230e2e4cf5ea388f2598a19249aa7fbc
K.Sidorov <2887458@gmail.com>
2023-02-20 00:17:23 +0300

    ended the solution fo days off

--- Homework-12-02-2023/days-off/Program.cs
+++ Homework-12-02-2023/days-off/Program.cs
@@ -1,15 +1,27 @@
 ﻿
 // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int number;
+Console.Write($"Введите цифру дня недели: ");
+int.TryParse(Console.ReadLine(), out number);
 
-int number = 7;
-Console.Write($"Введите цифру дня недели: {number}");
+bool condition3 = number < 1;
+bool condition4 = number > 7;
 
-
-if (number == 6)
+if (condition3 || condition4)
 {
-    Console.WriteLine(" -> этот день - выходной!");
+    Console.WriteLine($"Вы ввели некорректный день недели. Введите цифры от 1 до 7!");
 }
 else
 {
-    Console.WriteLine(" -> этот день - рабочий =(");
+    bool condition1 = number == 6;
+    bool condition2 = number == 7;
+
+    if (condition1 || condition2)
+    {
+        Console.WriteLine($"{number} -> этот день - выходной!");
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> этот день - рабочий =(");
+    }
 }
