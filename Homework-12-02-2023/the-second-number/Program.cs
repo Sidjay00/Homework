e4b506c6cdd65e5d66a03ada38ee6941e4cdd06f
K.Sidorov <2887458@gmail.com>
2023-02-19 14:10:53 +0300

    added the solution for the second number

--- Homework-12-02-2023/the-second-number/Program.cs
+++ Homework-12-02-2023/the-second-number/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает **вторую** цифру этого числа.
+
+int number;
+
+number = new Random().Next(100, 1000);
+
+int a = number % 100;
+int b = a / 10;
+
+Console.WriteLine($"{number} -> {b}");
