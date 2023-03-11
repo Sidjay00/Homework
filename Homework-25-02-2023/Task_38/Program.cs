9ad07d0a97c4d5974e9c0c8ff618eb306e7790e5
K.Sidorov <2887458@gmail.com>
2023-03-12 02:05:57 +0300

    so tired. dont understand, how it works

--- Homework-25-02-2023/Task_38/Program.cs
+++ Homework-25-02-2023/Task_38/Program.cs
@@ -2,26 +2,37 @@
 
 [3 7 22 2 78] -> 76 */
 
-double[] arr = FillArray(12, -100, 100);
+// /home/sysadmin/Dev/GeekBrains/Homework/Homework-25-02-2023/Task_38/Program.cs(11,8): error CS0161: 
+// "MaxMinDif(double[])": не все пути к коду возвращают значение. 
+// [/home/sysadmin/Dev/GeekBrains/Homework/Homework-25-02-2023/Task_38/Task_38.csproj]
+// периодически возникает такая ошибка - это происходит из-за того, что я return ставлю внутри цикла for. NB!
 
+double[] arr = FillArray(4, -100, 100);
 
-// System.Console.WriteLine($"[{String.Join(", ", arr)}]");
+System.Console.WriteLine($"[{String.Join(", ", arr)}] -> {MaxMinDif(arr)}");
+
+double MaxMinDif(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    double dif = 0;
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        
+        if (min < arr[i]) min = arr[i];
+        if (max > arr[i]) max = arr[i];
+    }
+    
+    System.Console.WriteLine($"{max}; {min}");
+    dif = max - min;
+    return dif;
+}
 
 double[] FillArray(int size, int min = 0, int max = 100)
 {
-double[] arr = new double[size];
-for (int i = 0; i < size; i++)
-    arr[i] = new Random().Next(min, max);
-return arr;
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
 }
-
-System.Console.WriteLine($"[{String.Join(", ", arr)}]");
-
-
-// double[] FillArray(int size, double min = 0, double max = 100)
-// {
-//     double[] arr = new int[size];
-//     for (double i = 0; i < size; i++)
-//         arr[i] = new Random().Next(min, max);
-//     return arr;
-// }
