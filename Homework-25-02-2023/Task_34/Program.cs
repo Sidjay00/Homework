4395a7dcf502ae6525e3b66fde975ea7fe27caa7
K.Sidorov <2887458@gmail.com>
2023-03-11 13:58:32 +0300

    added solution for Task 34

---
+++ Homework-25-02-2023/Task_34/Program.cs
@@ -0,0 +1,22 @@
+﻿/* **Задача 34:** Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+int[] array = FillArray(12, 100, 1000);
+int Odds = 0;
+
+foreach (int item in array)   //Здесь item - это элемент массива.
+{
+    if (item % 2 == 0)
+        Odds += 1;
+}
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> количество четных чисел в массиве - {Odds}");
+
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
