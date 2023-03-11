87e9b4bdc68987937a981737edc743f654c65a5a
K.Sidorov <2887458@gmail.com>
2023-03-12 01:08:34 +0300

    added methon for filling an array

--- Homework-25-02-2023/Task_38/Program.cs
+++ Homework-25-02-2023/Task_38/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* **Задача 38:** Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+double[] arr = FillArray(12, -100, 100);
+
+
+// System.Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+double[] FillArray(int size, int min = 0, int max = 100)
+{
+double[] arr = new double[size];
+for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(min, max);
+return arr;
+}
+
+System.Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+
+// double[] FillArray(int size, double min = 0, double max = 100)
+// {
+//     double[] arr = new int[size];
+//     for (double i = 0; i < size; i++)
+//         arr[i] = new Random().Next(min, max);
+//     return arr;
+// }
