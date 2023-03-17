692a542ebe5257e966f55d0b04645943f6f4c256
K.Sidorov <2887458@gmail.com>
2023-03-17 21:56:57 +0300

    added the solution for task 47

--- Homework-04-03-2023/Task_47/Program.cs
+++ Homework-04-03-2023/Task_47/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/* **Задача 47:** Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9 */
+
+ double[,] array = new double[3, 4];
+ for (int i = 0; i < array.GetLength(0); i++) //Строки
+ {
+    for (int j = 0; j < array.GetLength(1); j++) // Столбцы
+    {
+        array[i, j] = new Random().Next(-10, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    // Console.WriteLine(array.GetType());  // проверял, какие значения записаны в массив - целые или вещественные
+}
