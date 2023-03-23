601eecbb3a7a8a23e43fa8d4217e73fa7fdddd43
K.Sidorov <2887458@gmail.com>
2023-03-23 21:57:35 +0300

    refactored a little

--- Homework-04-03-2023/Task_47/Program.cs
+++ Homework-04-03-2023/Task_47/Program.cs
@@ -9,13 +9,14 @@ m = 3, n = 4.
 
 8 7,8 -7,1 9 */
 
- double[,] array = new double[3, 4];
- for (int i = 0; i < array.GetLength(0); i++) //Строки
- {
+double[,] array = new double[3, 4];
+for (int i = 0; i < array.GetLength(0); i++) //Строки
+{
     for (int j = 0; j < array.GetLength(1); j++) // Столбцы
     {
-        array[i, j] = new Random().Next(-10, 10);
+        array[i, j] = new Random().Next(-10, 10) + Math.Round(new Random().NextDouble(), 3);
         Console.Write($"{array[i, j]} ");
     }
+    Console.WriteLine();
     // Console.WriteLine(array.GetType());  // проверял, какие значения записаны в массив - целые или вещественные
 }
