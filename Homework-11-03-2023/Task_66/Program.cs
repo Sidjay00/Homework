0045548ca2d86787cc82fec8f5bf870e288cff88
K.Sidorov <2887458@gmail.com>
2023-04-01 11:47:36 +0300

    solution for task 66

--- Homework-11-03-2023/Task_66/Program.cs
+++ Homework-11-03-2023/Task_66/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  **Задача 66:** Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+int m, n;
+Console.Write($"Введите число M: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write($"Введите число N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.WriteLine($"M = {m}; N = {n} -> {PrintSum(m, n)}");
+
+int PrintSum(int m, int n)
+{
+    if (m >= n)
+        return m;
+    return m += PrintSum(m + 1, n); //  Снова пишу для себя, так как совершил ошибку на переполнение. Нельзя вызывать рекурсию без изменения переменной, иначе будет зацикливание
+}
